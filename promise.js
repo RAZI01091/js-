e00818400986409798d2a2fcdedefee0da548365
RAZI01091 <muhammedrazi01091@gmail.com>
2026-08-06 12:30:43 +0530

    Initial commit

---
+++ promise.js
@@ -0,0 +1,13 @@
+let promise = new Promise(function(resolve, reject) {
+
+    console.log("Loading...");
+
+    setTimeout(function() {
+        resolve("Data Loaded Successfully");
+    }, 3000);
+
+});
+
+promise.then(function(result) {
+    console.log(result);
+});
