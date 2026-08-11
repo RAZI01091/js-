21f1340d89f8e8fe0ade2f8ecc3e961926fc11f2
RAZI01091 <muhammedrazi01091@gmail.com>
2026-08-11 12:10:59 +0530

    datatypes

--- datatype.js
+++ datatype.js
@@ -1,3 +1,30 @@
+// string
+
 let name="razi";
 console.log(name);
 console.log(typeof name)
+
+// number
+
+let age=19;
+console.log(age);
+console.log(typeof age);
+
+// boolean
+
+let isStudent=true;
+console.log(true);
+console.log(typeof isStudent);
+
+
+// undefined
+
+let nothing;
+console.log(nothing);
+console.log(typeof nothing)
+
+// null   
+
+let phone=null;
+console.log(phone);
+console.log(typeof phone);
